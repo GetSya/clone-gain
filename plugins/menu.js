25f620d0fe5eceb8016ebb6fd65aa4219318aa2c
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2021-12-11 14:12:30 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -40,8 +40,7 @@ const defaultMenu = {
 │
 │ Uptime: *%uptime (%muptime)*
 │ Database: %rtotalreg of %totalreg
-│ Github:
-│ %github
+│ Github: https://github.com/GetSya
 ╰────
 %readmore`.trimStart(),
   header: '╭─「 %category 」',
